8501fb8cd0b2ef70df5a4e4d4334b756c826b9b9
Madeleine Genova <madiane@unc.edu>
2023-04-28 00:48:11 -0400

    first pass at api calls

--- server.js
+++ server.js
@@ -1,1 +1,50 @@
-import { rps, rpsls } from "./lib/rpsls.js"
+#!/usr/bin/env node
+import { rps, rpsls } from "./lib/rpsls.js";
+import minimist from 'minimist';
+import express from 'express';
+
+const args = minimist(process.argv.slice(2)); 
+const port = args.port || 5000;
+const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get('/app/', (req, res) => {
+    res.status(200)
+    res.send("200 OK");
+})
+
+app.get('/app/rps/', (req, res) => {
+    res.status(200)
+    res.send(rps());
+})
+
+app.get('/app/rpsls/', (req, res) => {
+    res.status(200)
+    res.send(rpsls());
+})
+
+app.get('/app/rps/play/', (req, res) => {
+    res.status(200)
+    res.send(rps(req.query.shot))
+})
+
+app.get('/app/rpsls/play/', (req, res) => {
+    res.status(200)
+    res.send(rpsls(req.query.shot))
+})
+
+app.get('/app/rps/play/' + shot, (req, res) => {
+    res.status(200)
+    res.send(rps(shot))
+})
+
+app.get('/app/rpsls/play/' + shot, (req, res) => {
+    res.status(200)
+    res.send(rpsls(shot))
+})
+
+app.get("*", (req, res) => {
+    res.status(404)
+    res.send("404 NOT FOUND");
+})
